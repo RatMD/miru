daaceaa9a5387f22d779dd4b8f878e26d0e75f18
Sam <sam@rat.md>
2024-03-03 21:28:29 +0100

    [TASK] Add equals utility + [FIX] useForm composable

--- src/composables/use-form.ts
+++ src/composables/use-form.ts
@@ -3,10 +3,13 @@ import type { ComputedRef, MaybeRef, Ref, UnwrapNestedRefs } from 'vue';
 import type * as zod from 'zod';
 import type { ZodTypeAny, SafeParseReturnType } from 'zod';
 
-import { computed, reactive, ref, unref } from 'vue';
+import { computed, toRaw, reactive, ref, unref } from 'vue';
 import { z } from 'zod';
+import equals from '@/utils/equals';
 
-export type ValidFormMethods = 'GET' | 'PATCH' | 'POST' | 'PUT';
+export type FormMethods = 'GET' | 'PATCH' | 'POST' | 'PUT';
+
+export type Validator<T> = (z: typeof zod, values: T) => ValidationRules<T>;
 
 export type ValidationResult<T> = { 
     valid: boolean,
@@ -36,7 +39,7 @@ export interface FormHandler<T> {
     /**
      * The form method value, when useForm is called with an form element, otherwise 'POST'.
      */
-    method: ComputedRef<ValidFormMethods>;
+    method: ComputedRef<FormMethods>;
 
     /**
      * The passed, original initial values.
@@ -84,16 +87,58 @@ export interface FormHandler<T> {
     formData: ComputedRef<FormData>;
 
     /**
+     * Set Form Validator handler.
+     * @param validator 
+     * @returns
+     */
+    setValidator(validatorFunc: Validator<T>): void;
+
+    /**
+     * Unset Form Validator handler.
+     * @returns
+     */
+    unsetValidator(): void;
+
+    /**
+     * Touch form to trigger validation handling.
+     * @returns
+     */
+    touch(): void;
+
+    /**
+     * Untouch form to reset validation handling.
+     * @returns
+     */
+    untouch(): void;
+
+    /**
      * Check if a specific field is dirty.
      * @param key 
      */
     isDirty(key: keyof T): boolean;
 
     /**
-     * Validate Form Values
+     * Validate a specific field.
+     * @param key
+     * @param force
      * @returns 
      */
-    validate(): ValidationResult<T>;
+    isValid(key: keyof T, force?: boolean): boolean;
+
+    /**
+     * Validate a specific field.
+     * @param key
+     * @param force
+     * @returns 
+     */
+    isInvalid(key: keyof T, force?: boolean): boolean;
+
+    /**
+     * Validate Form Values
+     * @param keys Limit validation to the passed set of keys.
+     * @returns 
+     */
+    validate(keys?: Array<keyof T>): ValidationResult<T>;
 
     /**
      * Submit Form handler.
@@ -124,8 +169,10 @@ export interface FormHandler<T> {
 export function useForm<T extends object>(
     element: MaybeRef<HTMLFormElement|undefined>, 
     initial: T,
-    rules: (z: typeof zod) => ValidationRules<T>
+    rules?: Validator<T>
 ): FormHandler<T> {
+    const validator = ref<Validator<T>|undefined>(rules ?? void 0);
+
     /**
      * Set Form Details
      */
@@ -136,7 +183,7 @@ export function useForm<T extends object>(
     const action = computed<string|null>(
         () => form.value ? form.value.getAttribute('action') : null
     );
-    const method = computed<ValidFormMethods>(
+    const method = computed<FormMethods>(
         () => ((form.value ? (form.value.getAttribute('method') || 'POST') : 'POST') as any).toUpperCase()
     );
 
@@ -144,9 +191,9 @@ export function useForm<T extends object>(
      * Set Form Values
      */
     const originals = Object.freeze(JSON.parse(JSON.stringify(initial)));
+    const keys = Object.freeze(Object.keys(initial)) as Array<keyof T>;
     const values = reactive<T>(initial);
     
-    const keys = Object.freeze(Object.keys(initial)) as Array<keyof T>;
     const dirtyKeys = computed<(keyof T)[]>(() => {
         const dirty: Array<keyof T> = [];
         for (const key of keys) {
@@ -155,7 +202,7 @@ export function useForm<T extends object>(
             }
         }
         return dirty;
-    })
+    });
 
     /**
      * State : Check if one or more fields are dirty.
@@ -195,25 +242,107 @@ export function useForm<T extends object>(
     });
 
     /**
+     * Set Form Validator handler.
+     * @param validator 
+     * @returns
+     */
+    function setValidator(validatorFunc: Validator<T>) {
+        validator.value = validatorFunc;
+    }
+
+    /**
+     * Unset Form Validator handler.
+     * @returns
+     */
+    function unsetValidator() {
+        validator.value = void 0;
+    }
+
+    /**
+     * Touch form to trigger validation handling.
+     * @returns
+     */
+    function touch() {
+        touched.value = true;
+    }
+
+    /**
+     * Untouch form to reset validation handling.
+     * @returns
+     */
+    function untouch() {
+        touched.value = false;
+    }
+
+    /**
      * Check if a specific field is dirty.
      * @param key 
      */
     function isDirty(key: keyof T) {
-        return originals[key] != (values as any)[key];
+        let original = originals[key];
+        let value = toRaw((values as any)[key]);
+        return !equals(original, value);
     }
 
     /**
-     * Validate Form Values
+     * Validate a specific field.
+     * @param key
+     * @param force
      * @returns 
      */
-    function validate(): ValidationResult<T> {
-        const ruleSet = rules(z as typeof zod);
+    function isValid(key: keyof T, force?: boolean): boolean {
+        if (!validator.value) {
+            return true;
+        }
+
+        // Always true when not touched & not changed (& not forced)
+        if (!touched.value && !(typeof force == 'boolean' && force === true)) {
+            if (!dirtyKeys.value.includes(key)) {
+                return true;
+            }
+        }
+
+        // Receive validation rules
+        const ruleSet = validator.value(z as typeof zod, payload.value);
+        return ruleSet[key].safeParse((values as any)[key]).success;
+    }
+
+    /**
+     * Validate a specific field.
+     * @param key
+     * @param force
+     * @returns 
+     */
+    function isInvalid(key: keyof T, force?: boolean): boolean {
+        return !isValid(key, force);
+    }   
+
+    /**
+     * Validate Form Values
+     * @param keys Limit validation to the passed set of keys.
+     * @returns 
+     */
+    function validate(keys?: Array<keyof T>): ValidationResult<T> {
+        if (!validator.value) {
+            return {
+                valid: true,
+                errors: {} as any,
+                results: {} as any
+            };
+        }
+        const ruleSet = validator.value(z as typeof zod, payload.value);
 
         let valid = true;
         let results: { [Property in keyof T]: SafeParseReturnType<any, any> } = {} as any;
         let errors: { [Property in keyof T]: string[] } = {} as any;
 
         for (const [key, rule] of Object.entries(ruleSet)) {
+            if (typeof keys != 'undefined') {
+                if (keys.indexOf(key as any) < 0) {
+                    continue;
+                }
+            }
+
             let result = (rule as ZodTypeAny).safeParse((values as any)[key]);
             
             if (!result.success) {
@@ -234,7 +363,7 @@ export function useForm<T extends object>(
 
     /**
      * Submit Form handler.
-     * @todo Handle Request Handler
+     * @todo Handle Request
      * @param newValues 
      * @returns 
      */
@@ -310,8 +439,16 @@ export function useForm<T extends object>(
         payload,
         formData,
 
+        // Handler
+        setValidator,
+        unsetValidator,
+        touch,
+        untouch,
+
         // Actions
         isDirty,
+        isValid,
+        isInvalid,
         validate,
         submit,
         update,
---
+++ src/utils/equals.ts
@@ -0,0 +1,108 @@
+
+
+function _equals(a: any, b: any, refs: any[]) {
+    const aType = Object.prototype.toString.call(a);
+    const bType = Object.prototype.toString.call(b);
+    let element;
+
+    // Compare primitives and referentially equal objects
+    if (a === b) {
+        return true;
+    }
+
+    // Compare null / undefined
+    if (a == null || b == null) {
+        return false;
+    }
+
+    // Check if reference have been seen before
+    if (refs.indexOf(a) > -1 && refs.indexOf(b) > -1) {
+        return true;
+    }
+    refs.push(a, b);
+
+    // Not the same type
+    if (aType != bType) {
+        return false;
+    }
+
+    // Check Properties
+    let aElements = Object.getOwnPropertySymbols ? Object.keys(a).concat(Object.getOwnPropertySymbols(a).map(i => i.toString())) : Object.keys(a);
+    let bElements = Object.getOwnPropertySymbols ? Object.keys(b).concat(Object.getOwnPropertySymbols(b).map(i => i.toString())) : Object.keys(b);
+    if (aElements.length != bElements.length || aElements.some((key) => _equals(a[key], b[key], refs))) {
+        return false;
+    }
+
+    // Check
+    switch (aType.slice(8, -1)) {
+        case "Symbol":
+            return a.valueOf() == b.valueOf();
+        case "Date":
+        case "Number":
+            return +a == +b || (+a != +a && +b != +b); // convert Dates to ms, check for NaN
+        case "RegExp":
+        case "Function":
+        case "String":
+        case "Boolean":
+            return "" + a == "" + b;
+        case "Set":
+        case "Map": {
+            aElements = a.entries();
+            bElements = b.entries();
+            do {
+                element = (aElements as any).next();
+                if (!_equals(element.value, (bElements as any).next().value, refs)) {
+                    return false;
+                }
+            } while (!element.done);
+            return true;
+        }
+        case "ArrayBuffer":
+            (a = new Uint8Array(a)), (b = new Uint8Array(b)); // fall through to be handled as an Array
+        case "DataView":
+            (a = new Uint8Array(a.buffer)), (b = new Uint8Array(b.buffer)); // fall through to be handled as an Array
+        case "Float32Array":
+        case "Float64Array":
+        case "Int8Array":
+        case "Int16Array":
+        case "Int32Array":
+        case "Uint8Array":
+        case "Uint16Array":
+        case "Uint32Array":
+        case "Uint8ClampedArray":
+        case "Arguments":
+        case "Array":
+            if (a.length != b.length) {
+                return false;
+            }
+            for (element = 0; element < a.length; element++) {
+                if (!(element in a) && !(element in b)) {
+                    continue; // empty slots are equal
+                }
+
+                // either one slot is empty but not both OR the elements are not equal
+                if (element in a != element in b || !_equals(a[element], b[element], refs)) {
+                    return false;
+                }
+            }
+            return true;
+        case "Object":
+            return _equals(Object.getPrototypeOf(a), Object.getPrototypeOf(b), refs);
+        default:
+            return false;
+    }
+}
+
+/**
+ * Equality Check
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+function equals(a: any, b: any) {
+    return _equals(a, b, []);
+}
+
+// Export Module
+export default equals;
+export { equals };
