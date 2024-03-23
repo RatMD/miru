e1b59a123738173301c24823677d234abd78be6f
Sam <sam@rat.md>
2024-03-23 21:14:55 +0100

    [FIX] story typing + v0.0.2 (experimental alpha)

--- src/components/button/ActionButton.story.vue
+++ src/components/button/ActionButton.story.vue
@@ -68,7 +68,7 @@ const icon = {
     props: {
         size: Number
     },
-    render(ctx) {
+    render(ctx: any) {
         return h('svg', {
             xmlns: "http://www.w3.org/2000/svg",
             width: ctx?.props?.size ?? 16,
