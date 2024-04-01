a5f52993e64bcb6aa950aa9e81fca27eb1639d9a
Sam <sam@rat.md>
2024-04-01 11:39:21 +0200

    [TASK] add now utility

---
+++ src/utils/now.ts
@@ -0,0 +1,22 @@
+
+/**
+ * Current Date/Time as simple DB iso string.
+ * @returns 
+ */
+function now(): string {
+    const date = new Date;
+    return [
+        date.getFullYear().toString(),
+        ('00' + date.getMonth()+1).slice(-2),
+        ('00' + date.getDate()).slice(-2)
+    ].join('-')
+    + ' ' +
+    [
+        ('00' + date.getHours()).slice(-2),
+        ('00' + date.getMinutes()).slice(-2),
+        ('00' + date.getSeconds()).slice(-2)
+    ].join(':')
+}
+
+export default now;
+export { now };
