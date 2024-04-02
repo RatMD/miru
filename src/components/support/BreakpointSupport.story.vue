984e37333bd46280c531e1a966082ab506b93f67
Sam <sam@rat.md>
2024-04-02 09:16:05 +0200

    [FIX] BreakpointSupport

--- src/components/support/BreakpointSupport.story.vue
+++ src/components/support/BreakpointSupport.story.vue
@@ -1,11 +1,39 @@
 <template>
     <Story title="Support/BreakpointSupport">
-        Work in Progress
+        <Variant title="Default">
+            <BreakpointSupport max="xxs">
+                xss
+            </BreakpointSupport>
+
+            <BreakpointSupport min="xxs" max="xs">
+                xxs - xs
+            </BreakpointSupport>
+
+            <BreakpointSupport min="xs" max="sm">
+                xs - sm
+            </BreakpointSupport>
+
+            <BreakpointSupport min="sm" max="md">
+                sm - md
+            </BreakpointSupport>
+
+            <BreakpointSupport min="md" max="lg">
+                md - lg
+            </BreakpointSupport>
+
+            <BreakpointSupport min="lg" max="xl">
+                lg - xl
+            </BreakpointSupport>
+
+            <BreakpointSupport min="xl" max="xxl">
+                xl - xxl
+            </BreakpointSupport>
+        </Variant>
     </Story>
 </template>
 
 <script lang="ts" setup>
-
+import BreakpointSupport from './BreakpointSupport.vue';
 </script>
 
 <docs lang="md">
