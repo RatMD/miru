f92b13f65589c9f0207d6b889ba37383e55e53fc
Sam <sam@rat.md>
2024-04-02 11:55:21 +0200

    [FIX] Bottom Offcanvas Placement

--- src/components/offcanvas/BaseOffcanvas.vue
+++ src/components/offcanvas/BaseOffcanvas.vue
@@ -319,7 +319,7 @@ defineExpose({
     }
     
     &.offcanvas-bottom {
-        @apply w-full left-0 right-0 bottom-0 -translate-y-full;
+        @apply w-full left-0 right-0 bottom-0 translate-y-full;
 
         &.is-visible {
             @apply translate-y-0;
